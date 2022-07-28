1ef9bab2409b558f36a4cc7e61b0dd220a0177ab
Jasur <jasur@example.com>
2022-07-28 15:08:16 +0500

    ...

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import ProductModel
+from django.views.generic import ListView, DetailView
+from .models import ProductModel, CategoryModel, ProductTagModel
 
 
 class ShopView(ListView):
@@ -8,4 +8,33 @@ class ShopView(ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        return ProductModel.objects.all()
+        qs = ProductModel.objects.all()
+
+        search = self.request.GET.get('search')
+        if search:
+            qs = qs.filter(title__icontains=search)
+
+        cat = self.request.GET.get('cat')
+        if cat:
+            qs = qs.filter(category_id=cat)
+
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tags=tag)
+        return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data()
+        data['categories'] = CategoryModel.objects.all()
+        data['tags'] = ProductTagModel.objects.all()
+        return data
+
+
+class ProductDetailView(DetailView):
+    model = ProductModel
+    template_name = 'shop-detail.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data()
+        data['products'] = ProductModel.objects.all().exclude(id=self.object.pk)[:4]
+        return data
