031fd145a16a44f711146b9f18f54406893b9f6a
Jasur <jasur@example.com>
2022-07-28 23:05:12 +0500

    sale

--- shop/signals.py
+++ shop/signals.py
@@ -5,8 +5,15 @@ from .models import ProductModel
 
 @receiver(pre_save, sender=ProductModel)
 def get_real_price(sender, instance, *args, **kwargs):
-
     if instance.is_discount():
         instance.real_price = ((100 - instance.discount) / 100) * instance.price
     else:
         instance.real_price = instance.price
+
+
+@receiver(pre_save, sender=ProductModel)
+def set_sale(sender, instance, *args, **kwargs):
+    if instance.is_discount():
+        instance.sale = True
+    else:
+        instance.sale = False
